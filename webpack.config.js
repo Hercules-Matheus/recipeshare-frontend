d50f8a63cb44415196fbea3c94ff402cafb081fe
Hercules Matheus Lima Silva <hercules.silva@parnamirim.rn>
2024-12-03 15:23:29 -0300

    update files

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
-  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+  ],
+  // outras configurações
 };
